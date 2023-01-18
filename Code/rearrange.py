223062e1990f6ab6fa663a6948e00cbd72add957
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-01-18 15:40:32 -0800

    Added rearrange module & moved files

---
+++ Code/rearrange.py
@@ -0,0 +1,13 @@
+import sys
+import random
+
+list_of_words = sys.argv[1:]
+
+
+def shuffle_words():
+     random.shuffle(list_of_words)
+
+# Wiuthout * returns a list
+if __name__ == "__main__":
+     shuffle_words()
+     print(*list_of_words)
