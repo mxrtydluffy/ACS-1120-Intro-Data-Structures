7ca09342793ee5af3a97c2a78a1b6b9858708f36
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-01-22 22:13:17 -0800

    declared functions

---
+++ Code/histogram.py
@@ -0,0 +1,19 @@
+# Analyze frequency of words in a body of text
+#find body of source text
+sentence = "one fish two fish red fish blue fish"
+
+def histogram(source_text):
+    """
+    Can be filename or the contents of the file as a string of choice.
+    """
+
+def unique_words(histogram):
+    """
+    Returns the total count of unique words in the histogram argument
+    and returns the total count of unique words in the histogram.
+    """
+
+def freuency(word, histogram):
+    """
+    Returns the number of times tht words appears in a text.
+    """
