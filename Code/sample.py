4bd5014ed15000720a8fa0317cb595d6abbd47f6
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-01-30 08:55:15 -0800

    fixed mistakes

--- Code/sample.py
+++ Code/sample.py
@@ -1,8 +1,7 @@
 import random
 from histogram import histogram
-import sys
 
-def random_word(histogram):
+def random_word(sentence):
     """
     Getting random word from the sentence
     """
@@ -11,16 +10,18 @@ def random_word(histogram):
         list(histogram_words.keys()),
         weights = histogram_words.values(),
         k = 1
-    )
+    )[0]
 
-    if __name__ == "__main__":
-        sentence = "one fish two fish red fish blue fish"
-        word_frequency = {}
-        for _ in range(3000000):
-            histogram_words = random_word(sentence)
-            if random_word in word_frequency:
-                word_frequency[random_word] += 1
-            else:
-                word_frequency[random_word] = 1
-        for word in word_frequency:
-            print(f"{word}: {word_frequency}")
+    return random_word
+
+if __name__ == "__main__":
+    sentence = "one fish two fish red fish blue fish"
+    word_frequency = {}
+    for _ in range(3000000):
+        histogram_words = random_word(sentence)
+        if random_word in word_frequency:
+            word_frequency[random_word] += 1
+        else:
+            word_frequency[random_word] = 1
+    for word in word_frequency:
+        print(f"{word}: {word_frequency[word]}")
