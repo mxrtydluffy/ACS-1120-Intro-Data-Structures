b9d5d231aa743736b40f95a7780576d13875e6c4
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-02-27 11:47:59 -0800

    [UPDATE] linkedlist test

--- Code/linkedlist_test.py
+++ Code/linkedlist_test.py
@@ -137,12 +137,13 @@ class LinkedListTest(unittest.TestCase):
         assert ll.head.data == 'A'  # New head
         assert ll.tail.data == 'C'  # Unchanged
 
+    # Gradescope
     def test_find(self):
         ll = LinkedList(['A', 'B', 'C'])
-        assert ll.find(lambda item: item == 'B') == 'B'  # Match equality
-        assert ll.find(lambda item: item < 'B') == 'A'  # Match less than
-        assert ll.find(lambda item: item > 'B') == 'C'  # Match greater than
-        assert ll.find(lambda item: item == 'X') is None  # No matching item
+        assert ll.find('B') == True  # Match equality
+        assert ll.find('A') == True  # Match less than
+        assert ll.find('C') == True  # Match greater than
+        assert ll.find('X') == False  # No matching item
 
     def test_delete_with_3_items(self):
         ll = LinkedList(['A', 'B', 'C'])
